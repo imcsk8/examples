4d018798e651b2804657859e6286788dad44c8bf
Iván Chavero <ivan.chavero@ugd.gob.mx>
2024-12-06 01:34:18 -0600

    Add event routes

--- posadev/src/main.rs
+++ posadev/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rocket;
+use rocket::fs::FileServer;
+use rocket::response::Debug;
+use rocket::{launch, routes};
+use rocket::get;
+use rocket_dyn_templates::Template;
+//https://rocket.rs/guide/v0.5/requests/
+
+type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
+
+pub mod auth;
+pub mod claims;
+pub mod db;
+pub mod models;
+pub mod schema;
+pub mod events;
+
+pub static STATIC_FILES_DIR: &str = "www/static";
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+		.mount("/", routes![auth::login])
+        .mount(
+            "/events",
+            routes![events::add, events::delete, events::get, events::list],
+        )
+        .mount("/public", FileServer::from(STATIC_FILES_DIR))
+        .attach(Template::fairing())
+        .attach(db::EventDB::fairing())
+
 }
